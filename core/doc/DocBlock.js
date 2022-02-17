d34ddc2c15d4c282cf6547406b00e882558da571
m.broersen <m.broersen@socialblue.com>
2022-02-17 22:04:31 +0100

    add doc description class

--- core/doc/DocBlock.js
+++ core/doc/DocBlock.js
@@ -1,5 +1,5 @@
 const DocProperty = require('./DocProperty');
-const DocPropertyTypes = require('./DocPropertyTypes');
+const DocDescription = require('./DocDescription');
 
 /**
  * @class
@@ -46,55 +46,11 @@ class DocBlock {
     }
 
     /**
-     * @todo optimize this part of the code.
-     * @return {{owner: string, access: string | undefined, kind: (string|string), name, is_generator: boolean, returns: string | undefined, params: string[]}}
+     *
+     * @return {DocDescription}
      */
     describe() {
-        const match = this._owner.match(/^(((?<access>(private)|(public)|(protected)) )?(?<is_static>(static)) )?(?<kind>((class)|(get)|(set)|(function)))?(?<is_method>(?<is_generator>\* )?[^(]+[(](?<params>[^)]*)[)](: ?(?<returns>[^{]+))? \{$)?(?<is_property>[^=]+\=)?/m)
-        let kind = match.groups?.is_method ? 'method' : null;
-        kind = match.groups?.is_property ? 'property' : kind;
-        let access = 'public';
-        let example = null;
-        let description = null;
-        let returns = null;
-
-        for (const property of this.properties()) {
-            if (Object.values(DocPropertyTypes.access).includes(property.name)) {
-                access = property.name;
-            }
-
-            if (Object.values(DocPropertyTypes.kind).includes(property.name)) {
-                kind = property.name;
-            }
-
-            if (property.name === 'example') {
-                example = property.content;
-            }
-
-            if (property.name === 'description') {
-                description = property.content;
-            }
-
-            if (property.name === 'return') {
-                returns = property.content;
-            }
-        }
-
-        //todo cleanup
-        return {
-            owner: this._owner,
-            name: this.ownerName,
-            access: match.groups?.access ?? access,
-            kind: match.groups?.kind ?? kind,
-            is_generator: !!match.groups?.is_generator,
-            is_static: !!match.groups?.is_static,
-            returns: match.groups?.returns ?? returns,
-            example,
-            description,
-            params: (match.groups?.params ?? '')
-                .split(',')
-                .filter((value) => value !== ''),
-        }
+        return new DocDescription(this).describe();
     }
 
     /**
---
+++ core/doc/DocDescription.js
@@ -0,0 +1,101 @@
+const DocPropertyTypes = require("./DocPropertyTypes");
+
+/**
+ * @author Mark Broersen
+ * @licence MIT
+ * @class
+ */
+class DocDescription {
+
+    /**
+     *
+     * @param {DocBlock} docBlock
+     */
+    constructor(docBlock) {
+        this.docBlock = docBlock;
+        this.data = {
+            access: 'public',
+            author: null,
+            copyright: null,
+            description: null,
+            example: null,
+            is_generator: false,
+            is_static: false,
+            kind: null,
+            licence: null,
+            name: null,
+            params: [],
+            returns: null,
+            see: null,
+        }
+    }
+
+    /**
+     *
+     * @return {RegExpMatchArray}
+     */
+    get ownerParts() {
+        return this.docBlock._owner.match(/^(((?<access>(private)|(public)|(protected)) )?(?<is_static>(static)) )?(?<kind>((class)|(get)|(set)|(function)))?(?<is_method>(?<is_generator>\* )?[^(]+[(](?<params>[^)]*)[)](: ?(?<returns>[^{]+))? \{$)?(?<is_property>[^=]+\=)?/m)
+    }
+
+    /**
+     *
+     */
+    describeFromProperties() {
+        for (const property of this.docBlock.properties()) {
+            if (Object.values(DocPropertyTypes.access).includes(property.name)) {
+                this.data.access = property.name;
+            }
+
+            if (Object.values(DocPropertyTypes.kind).includes(property.name)) {
+                this.data.kind = property.name;
+            }
+
+            if (this.data.hasOwnProperty(property.name)) {
+                this.data[property.name] = property.content;
+            }
+        }
+    }
+
+    /**
+     *
+     * @param paramsMatch
+     */
+    describeParams(paramsMatch) {
+        this.data.params = (paramsMatch ?? '')
+            .split(',')
+            .filter((value) => value !== '')
+    }
+
+    /**
+     *
+     * @return {*|{access: string, is_static: boolean, kind: null, name: null, description: null, is_generator: boolean, returns: null, params: *[], example: null}}
+     */
+    describe() {
+        const match = this.ownerParts;
+
+        if (match.groups?.kind) {
+            this.data.kind = match.groups?.kind;
+        }
+
+        if (match.groups?.is_method) {
+            this.data.kind = 'method';
+        }
+
+        if (match.groups?.is_property) {
+            this.data.kind = 'property';
+        }
+        this.data.name = this.docBlock.ownerName;
+        this.describeParams(match.groups.params);
+        this.describeFromProperties();
+
+        return this.data;
+    }
+
+    toJSON() {
+        return this.data;
+    }
+
+}
+
+module.exports = DocDescription;
